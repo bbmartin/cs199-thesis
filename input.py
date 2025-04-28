8a276f39e1d40e4013fe8eef9d3bbd6b838c1039
bbmartin <bbmartin@up.edu.ph>
2025-04-28 10:43:53 +0800

    Add flags for context, change translation for if statements, deleted rules.txt

--- input.py
+++ input.py
@@ -1,7 +1,21 @@
-#f: x = "green"
+#s0: str
+#v0: x = "yellow"
+#p0: {"green", "yellow", "red"}
+x = "yellow"
 if x == "green":
-    return "yellow"
+    x = "yellow"
 elif x == "yellow":
-    return "red"
+    x = "red"
 else:
-    return "green"
+    x = "green"
+
+#s1: int
+#v1: y = 0
+y = 0
+for i in range(10):
+    y = y + i
+
+#s2: int
+#v2: z = 0
+while z < 10:
+    z = z + 1
